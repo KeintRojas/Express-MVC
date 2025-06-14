6c0ade979308820fa172ffd90412d040658672ec
Keint Rojas <keintrojas@gmail.com>
2025-06-14 10:34:56 -0600

    Actualizacion

---
+++ KFD/Models/Dish.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.SqlTypes;
+
+namespace KFD.Models
+{
+    public class Dish
+    {
+        public Dish()
+        {
+        }
+
+        public Dish(int id, string name, string description
+            , SqlMoney price, string picture, SqlBoolean isEnabled)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Price = price;
+            Picture = picture;
+            IsEnabled = isEnabled;
+        }
+
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public SqlMoney Price { get; set; }
+        [Required]
+        public string Picture { get; set; }
+        public SqlBoolean IsEnabled { get; set; }
+
+    }
+}
---
+++ KFD/Models/User.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlTypes;
+using System.Runtime.InteropServices;
+
+namespace KFD.Models
+{
+    public abstract class User
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [PasswordPropertyText]
+        [Length(8,12)]
+        public string Password { get; set; }
+        [Required]
+        public string Rol {  get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public SqlBoolean IsEnabled { get; set; }
+        public User()
+        {
+        }
+
+        protected User(int id, string name, string password, string rol
+            , string email, string address, SqlBoolean isEnabled)
+        {
+            Id = id;
+            Name = name;
+            Password = password;
+            Rol = rol;
+            Email = email;
+            Address = address;
+            IsEnabled = isEnabled;
+        }
+    }
+}
