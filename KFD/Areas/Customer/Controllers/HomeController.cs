d94797f6ff07097a6ab004f53371d96495ebe058
Keint Rojas <keintrojas@gmail.com>
2025-07-04 13:10:35 -0600

    Nueva Area del cliente

---
+++ KFD/Areas/Customer/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using KFD.Data.Repository.Interfaces;
+using KFD.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KFD.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class HomeController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public HomeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Dish> dishList = _unitOfWork.Dish.GetAll();
+            return View(dishList);
+        }
+
+    }
+}
