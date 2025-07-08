fca40e8b36cd640fe35ae0c907b0aa9ea0eb2c09
Keint Rojas <keintrojas@gmail.com>
2025-07-08 11:33:32 -0600

    update

---
+++ KFD/Migrations/20250708173248_update.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KFD.Migrations
+{
+    /// <inheritdoc />
+    public partial class update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "clientID",
+                table: "orders");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerId",
+                table: "orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomerId",
+                table: "orders");
+
+            migrationBuilder.AddColumn<int>(
+                name: "clientID",
+                table: "orders",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
